f2ce09a5e132229c2859c48cf63eac7c5bcaf226
corey <coreybatchler7@gmail.com>
2025-09-18 06:48:27 +1000

    split recipe modal into new component

--- app/components/RecipeCard.tsx
+++ app/components/RecipeCard.tsx
@@ -1,44 +1,9 @@
 import { useState } from "react";
 import { Recipe } from "../framework/schema";
-import Modal from "./Modal";
-import { useCookingMode } from "./CookingModeContext";
+import RecipeModal from "./RecipeModal";
 
 const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { cookingMode, toggleCookingMode } = useCookingMode();
-
-  const RecipeDetails = () => (
-    <>
-      <div className="text-gray-700 mb-4">
-        <div className="flex flex-wrap gap-x-4 gap-y-1 text-sm text-gray-600">
-          <span><span className="font-medium">Calories:</span> {recipe.calories} kCal</span>
-          <span><span className="font-medium">Protein:</span> {recipe.protein} g</span>
-          <span><span className="font-medium">Carbs:</span> {recipe.carbs} g</span>
-          <span><span className="font-medium">Fat:</span> {recipe.fat} g</span>
-        </div>
-      </div>
-
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">Ingredients</h3>
-      <ul className="list-disc list-inside text-gray-700 mb-6 space-y-2">
-        {recipe.ingredients
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .sort((a, b) => Number(b.isStaple) - Number(a.isStaple))
-          .map(i => (
-            <li key={i.name} className="flex justify-between items-center">
-              <span className={i.isStaple ? "font-bold" : "font-medium"}>{i.name}</span>
-              <span>{i.amount} {i.unit}</span>
-            </li>
-          ))}
-      </ul>
-
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">Method</h3>
-      <ol className="list-decimal list-inside text-gray-700 space-y-2">
-        {recipe.steps.map((s, index) => (
-          <li key={index}>{s}</li>
-        ))}
-      </ol>
-    </>
-  );
 
   return (
     <>
@@ -55,24 +20,7 @@ const RecipeCard = ({ recipe }: { recipe: Recipe }) => {
         </div>
       </div>
 
-      <Modal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        title={recipe.name}
-      >
-        <label className="inline-flex items-center cursor-pointer mb-6">
-          <input
-            type="checkbox"
-            className="sr-only peer"
-            checked={cookingMode}
-            onChange={toggleCookingMode}
-          />
-          <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-blue-600 after:absolute after:top-[2px] after:start-[2px] after:w-5 after:h-5 after:bg-white after:rounded-full after:transition-all peer-checked:after:translate-x-full"></div>
-          <span className="ms-3 text-sm font-medium text-gray-900">Cooking mode</span>
-        </label>
-
-        <RecipeDetails />
-      </Modal>
+      <RecipeModal recipe={recipe} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
     </>
   );
 };
---
+++ app/components/RecipeModal.tsx
@@ -0,0 +1,62 @@
+import { Recipe } from "../framework/schema";
+import { Dispatch, SetStateAction } from "react";
+import { useCookingMode } from "./CookingModeContext";
+import Modal from "./Modal";
+
+interface RecipeModalProps {
+    recipe: Recipe,
+    isModalOpen: boolean,
+    setIsModalOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const RecipeModal = ({ recipe, isModalOpen, setIsModalOpen }: RecipeModalProps) => {
+    const { cookingMode, toggleCookingMode } = useCookingMode();
+
+    return <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        title={recipe.name}
+    >
+        <label className="inline-flex items-center cursor-pointer mb-6">
+            <input
+            type="checkbox"
+            className="sr-only peer"
+            checked={cookingMode}
+            onChange={toggleCookingMode}
+            />
+            <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-checked:bg-blue-600 after:absolute after:top-[2px] after:start-[2px] after:w-5 after:h-5 after:bg-white after:rounded-full after:transition-all peer-checked:after:translate-x-full"></div>
+            <span className="ms-3 text-sm font-medium text-gray-900">Cooking mode</span>
+        </label>
+
+        <div className="text-gray-700 mb-4">
+            <div className="flex flex-wrap gap-x-4 gap-y-1 text-sm text-gray-600">
+            <span><span className="font-medium">Calories:</span> {recipe.calories} kCal</span>
+            <span><span className="font-medium">Protein:</span> {recipe.protein} g</span>
+            <span><span className="font-medium">Carbs:</span> {recipe.carbs} g</span>
+            <span><span className="font-medium">Fat:</span> {recipe.fat} g</span>
+            </div>
+        </div>
+
+        <h3 className="text-xl font-semibold text-gray-800 mb-2">Ingredients</h3>
+        <ul className="list-disc list-inside text-gray-700 mb-6 space-y-2">
+            {recipe.ingredients
+            .sort((a, b) => a.name.localeCompare(b.name))
+            .sort((a, b) => Number(b.isStaple) - Number(a.isStaple))
+            .map(i => (
+                <li key={i.name} className="flex justify-between items-center">
+                <span className={i.isStaple ? "font-bold" : "font-medium"}>{i.name}</span>
+                <span>{i.amount} {i.unit}</span>
+                </li>
+            ))}
+        </ul>
+
+        <h3 className="text-xl font-semibold text-gray-800 mb-2">Method</h3>
+        <ol className="list-decimal list-inside text-gray-700 space-y-2">
+            {recipe.steps.map((s, index) => (
+            <li key={index}>{s}</li>
+            ))}
+        </ol>
+    </Modal>
+};
+
+export default RecipeModal;
