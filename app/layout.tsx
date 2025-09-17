e0166f4a0418d21d3944cb5c5f3be092c430a2f4
Corey Batchler <coreybatchler7@gmail.com>
2025-09-17 21:30:18 +1000

    fix favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,9 +31,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png"></link>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"></link>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"></link>
         <link rel="manifest" href="/site.webmanifest"></link>
       </head>
       <body
